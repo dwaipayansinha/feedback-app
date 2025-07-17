6f7e73bbc3e0577b5dd12eac5ca064713059df3a
Dwaipayan Sinha <sinha.dwaipayan@gmail.com>
2025-07-17 19:20:11 -0400

    Updated react setup to the frontend and updated README

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://feedback-api-1063311936337.us-central1.run.app";
 
 function App() {
   const [feedbacks, setFeedbacks] = useState([]);
